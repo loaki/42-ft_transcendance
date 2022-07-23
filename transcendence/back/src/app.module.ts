c956924813db3171bf89acf79b170fd10e31ce65
Jules FEUILLATRE <jfeuilla@e1r1p11.clusters.42paris.fr>
2022-07-24 00:55:19 +0200

    ok

--- transcendence/back/src/app.module.ts
+++ transcendence/back/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 // import { AppGateway } from './app.gateway';
 import { GameGateway } from './game/game.gateway';
-import { AlertGateway } from './alert/alert.gateway';
-import { AlertController } from './alert/alert.controller';
 
 @Module({
   imports: [],
-  controllers: [AlertController],
-  providers: [GameGateway, AlertGateway],
+  controllers: [],
+  providers: [GameGateway],
 })
 export class AppModule {}
